35d53ea24b27a4e61ba6b414e19d408083f6bc71
bnyt_jane <bnyt_jane@163.com>
2019-11-22 13:46:03 +0800

    update single linked test add depends

--- tests/DataStructure/SingleLinkedListTest.php
+++ tests/DataStructure/SingleLinkedListTest.php
@@ -16,20 +16,48 @@ class SingleLinkedListTest extends TestCase
         parent::setUp();
     }
 
-    public function testInsert(){
+    public function testInsert()
+    {
         $this->linkedlist->insert('skylar');
-        $this->assertEquals('skylar',$this->linkedlist->get(1));
+        $this->assertEquals('skylar', $this->linkedlist->get(1));
     }
 
-    public function testSet(){
-        $this->linkedlist->set(1,'one');
-        $this->assertEquals('one',$this->linkedlist->get(1));
+    public function testSet()
+    {
+        $this->linkedlist->set(1, 'one');
+        $this->assertEquals('one', $this->linkedlist->get(1));
     }
 
-    public function testDelete(){
+    public function testDelete()
+    {
         $this->linkedlist->delete(1);
         $this->assertEmpty($this->linkedlist->getLen());
     }
 
+    public function testEmpty()
+    {
+        $this->assertEmpty($this->linkedlist->getLen());
+        return $this->linkedlist;
+    }
 
+    /**
+     * @depends testEmpty
+     */
+    public function testInsert2($linkedlist)
+    {
+        $linkedlist->insert('notempty');
+        $this->assertNotEmpty($linkedlist->getLen());
+        $this->assertEquals('notempty', $linkedlist->get(1));
+        return $linkedlist;
+    }
+
+    /**
+     * @depends testInsert2
+     */
+    public function testDelete2($linkedlist)
+    {
+        $this->assertEquals(true, $linkedlist->delete(1));
+        $this->assertEmpty($linkedlist->getLen());
+        $this->assertEquals(null, $linkedlist->get(1));
+    }
 }
